c404bb7a134c3aa95acf423c145dd2ddaf75c25b
Russell Schoen <rschoen@oreillyauto.com>
2019-11-12 08:45:55 -0600

    Initial commit

---
+++ src/components/DataComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="data-component">
+    <h3>TODO: Create view of data here</h3>
+    <p>{{ data }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"DataComponent",
+  props: {
+    data: String
+  }
+}
+</script>
---
+++ src/views/DataView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="data-view">
+    <DataComponent data='some data'/>
+  </div>
+</template>
+
+
+<script>
+import DataComponent from '@/components/DataComponent.vue'
+
+export default {
+  name: 'data-view',
+  components: {
+    DataComponent
+  }
+}
+
+
+</script>
