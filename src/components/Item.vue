8c0894bb386323db1fa9dd23889836c054aa1cb7
Russell Schoen <rschoen@oreillyauto.com>
2019-11-12 16:21:41 -0600

    Added bootstrap, tables, etc

---
+++ src/components/Item.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class='item'>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        line: String,
+        Item: String,
+        netprics: String
+    }
+}
+</script>
---
+++ src/util/itemConstants.js
@@ -0,0 +1,2 @@
+export const BASE_URL = ''
+export const ALL_ITEMS = BASE_URL + '/item-list.json'
