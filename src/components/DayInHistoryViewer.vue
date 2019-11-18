6b2045f160d6386549ba1c47cf4c5fa3209da80a
Russell Schoen <rschoen@oreillyauto.com>
2019-11-18 07:25:22 -0600

    Reverted last changes

--- src/components/DayInHistoryViewer.vue
+++ src/components/DayInHistoryViewer.vue
@@ -2,7 +2,7 @@
   <div id="app" class="container">
     <h1>On This Day</h1>
     <h3>Provided by Wikipedia</h3>
-    <!--<datepicker 
+    <datepicker 
       id='datepicker'
       v-model="model.date"
       :inline='false' 
@@ -11,33 +11,22 @@
       popover-align="center"
       @selected="updateDate($event)"
       @opened="updateDate($event)">
-    </datepicker>-->
-    <v-date-picker
-      id="datepicker"
-       :popover="{ placement: 'bottom', visibility: 'click' }"
-      v-model="model.date"
-      :popover-align:center="true"
-      :inlign='false' 
-      :attributes='model.attributes'
-      @click="updateDate($event)"/>
-    <day-in-history />
+    </datepicker>
+    <DayInHistory />
   </div>
 </template>
 
 <script>
 import DayInHistory from './DayInHistory.vue'
 import { FULL_URL } from '../util/constants.js'
+import Datepicker from 'vuejs-datepicker'
 
 export default {
   data() {
     return {
       name: "russell",
       model: {
-        attributes: [{
-          key: 'today'
-        }],
         date: new Date(),
-        mode: 'single',
         highlighted: {
           dates: [
             new Date()
@@ -47,7 +36,9 @@ export default {
     }
   },
   components: {
-    DayInHistory
+    DayInHistory,
+    Datepicker
+
   },
   methods: {
     updateDate: function(date) {
